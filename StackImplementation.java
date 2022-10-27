0c0457869f8c34a0353afb9731a95e188e471b12
dsdhumale <d.sdhumale@gmail.com>
2022-10-27 20:15:28 +0530

    Added : Ability to push elements in stack

---
+++ StackImplementation.java
@@ -0,0 +1,36 @@
+package com.Bridgelabz.StackandQueue;
+
+public class StackImplementation {
+	private Node top;
+
+	public StackImplementation() {
+		this.top = top;
+	}
+//Method for pushing element in stack
+	public void push(int element) {
+
+		Node node = new Node();
+		node.data = element;
+		if (node == null) {
+			node.next = null;
+		} else {
+			node.next = top;
+			top = node;
+		}
+
+	}
+	//Method for displaying elements of stack
+	public void display() {
+		if (top == null)
+			System.out.println("Stack is empty");
+		else {
+			Node temp = top;
+			while (temp.next != null) {
+				System.out.print(temp.data + " -> ");
+				temp = temp.next;
+			}
+			System.out.println(temp.data);
+
+		}
+	}
+}
---
+++ StackMain.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.StackandQueue;
+
+public class StackMain {
+	public static void main(String[] args) {
+		StackImplementation stack = new StackImplementation();
+		stack.display();
+		stack.push(70);
+		stack.display();
+		stack.push(30);
+		stack.display();
+		stack.push(56);
+		stack.display();
+
+	}
+}
